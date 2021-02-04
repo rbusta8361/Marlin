5bea46d644450f8f1893118b479f15ec8f9e127e
rbusta8361 <77689525+rbusta8361@users.noreply.github.com>
2021-02-04 12:17:36 -0800

    Updated for 4.2.7 board

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -130,11 +130,11 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_CREALITY_V4
+  #define MOTHERBOARD BOARD_CREALITY_V427
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3 V2"
+#define CUSTOM_MACHINE_NAME "Ender-3 V2 4.2.7"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
